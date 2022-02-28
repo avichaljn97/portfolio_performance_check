789fa641f26e56f9c167172bc5af8c28aaf4614f
Avichal <56877090+avichaljn97@users.noreply.github.com>
2022-02-28 19:39:50 +0530

    Update canvas.js

--- chart-widget/canvas.js
+++ chart-widget/canvas.js
@@ -588,9 +588,11 @@ var col4=document.getElementsByClassName('LTP');
 var col5=document.getElementsByClassName('profitloss');
 var col6=document.getElementsByClassName('change');
 var col7=document.getElementsByClassName('dayChange');
+var sn;
 
 function disp(obj,stock_name,stock_desc){
 
+	sn=stock_name;
 	var stockdesc=document.getElementsByClassName("stockdesc")[0];
 
 	body.style.gridTemplateColumns="repeat(15,1fr)";
@@ -923,4 +925,149 @@ async function in_homepage(){
 		timeline.strokeStyle="rgb(75, 250, 177)";
 		timeline.stroke();		
 	}
+}
+
+var modalbox=document.getElementsByClassName('TransactModalBox')[0];
+var BuySell;
+function display_modalbox(obj,bs){
+	modalbox.style.display='block';
+	var footer=modalbox.getElementsByClassName('footer')[0];
+	var submit=footer.getElementsByClassName('submit')[0];
+	var header=modalbox.getElementsByClassName('header')[0];
+	header.innerHTML=sn;
+	BuySell=bs;
+	if(bs==1){
+		submit.innerHTML="Buy";
+	}
+	else{
+		submit.innerHTML="Sell";
+	}
+}
+function cancel(){
+	modalbox.style.display='none';
+}
+
+async function transact(obj){
+	var stockname=obj.parentNode.parentNode[0].value;
+	var date=obj.parentNode.parentNode[1].value;
+	var quantity=obj.parentNode.parentNode[2].value;
+	var price=obj.parentNode.parentNode[3].value;
+
+	var data={buysell:BuySell,stockname:stockname,date:date,quantity:quantity,price:price};
+	
+	var response= await fetch('./portfolio', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+  		},
+  		body: JSON.stringify(data),
+	});
+	const stat=await response.json();
+	if(stat.status_code==0){
+		location.href="/portfolio"
+	}
+}
+
+
+function autocomplete(inp, arr) {
+	/*the autocomplete function takes two arguments,
+	the text field element and an array of possible autocompleted values:*/
+	var currentFocus;
+	/*execute a function when someone writes in the text field:*/
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value;
+		/*close any already open lists of autocompleted values*/
+		closeAllLists();
+		if (!val) { return false;}
+		currentFocus = -1;
+		/*create a DIV element that will contain the items (values):*/
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		/*append the DIV element as a child of the autocomplete container:*/
+		this.parentNode.appendChild(a);
+		/*for each item in the array...*/
+		for (i = 0; i < arr.length; i++) {
+		  /*check if the item starts with the same letters as the text field value:*/
+		  if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+			/*create a DIV element for each matching element:*/
+			b = document.createElement("DIV");
+			/*make the matching letters bold:*/
+			b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+			b.innerHTML += arr[i].substr(val.length);
+			/*insert a input field that will hold the current array item's value:*/
+			b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+			/*execute a function when someone clicks on the item value (DIV element):*/
+				b.addEventListener("click", function(e) {
+				/*insert the value for the autocomplete text field:*/
+				inp.value = this.getElementsByTagName("input")[0].value;
+				/*close the list of autocompleted values,
+				(or any other open lists of autocompleted values:*/
+				closeAllLists();
+			});
+			a.appendChild(b);
+		  }
+		}
+	});
+	/*execute a function presses a key on the keyboard:*/
+	inp.addEventListener("keydown", function(e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) {
+		  /*If the arrow DOWN key is pressed,
+		  increase the currentFocus variable:*/
+		  currentFocus++;
+		  /*and and make the current item more visible:*/
+		  addActive(x);
+		} else if (e.keyCode == 38) { //up
+		  /*If the arrow UP key is pressed,
+		  decrease the currentFocus variable:*/
+		  currentFocus--;
+		  /*and and make the current item more visible:*/
+		  addActive(x);
+		} else if (e.keyCode == 13) {
+		  /*If the ENTER key is pressed, prevent the form from being submitted,*/
+		  e.preventDefault();
+		  if (currentFocus > -1) {
+			/*and simulate a click on the "active" item:*/
+			if (x) x[currentFocus].click();
+		  }
+		}
+	});
+	function addActive(x) {
+	  /*a function to classify an item as "active":*/
+	  if (!x) return false;
+	  /*start by removing the "active" class on all items:*/
+	  removeActive(x);
+	  if (currentFocus >= x.length) currentFocus = 0;
+	  if (currentFocus < 0) currentFocus = (x.length - 1);
+	  /*add class "autocomplete-active":*/
+	  x[currentFocus].classList.add("autocomplete-active");
+	}
+	function removeActive(x) {
+	  /*a function to remove the "active" class from all autocomplete items:*/
+	  for (var i = 0; i < x.length; i++) {
+		x[i].classList.remove("autocomplete-active");
+	  }
+	}
+	function closeAllLists(elmnt) {
+	  /*close all autocomplete lists in the document,
+	  except the one passed as an argument:*/
+	  var x = document.getElementsByClassName("autocomplete-items");
+	  for (var i = 0; i < x.length; i++) {
+		if (elmnt != x[i] && elmnt != inp) {
+		x[i].parentNode.removeChild(x[i]);
+	  }
+	}
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+	  closeAllLists(e.target);
+  });
+}
+async function initialautocomplete(){
+
+	var stocklist=await (await fetch("/nse_stocks/stock_meta.json")).json();
+	
+	autocomplete(document.getElementById("myInput"),Object.keys(stocklist) );
 }
